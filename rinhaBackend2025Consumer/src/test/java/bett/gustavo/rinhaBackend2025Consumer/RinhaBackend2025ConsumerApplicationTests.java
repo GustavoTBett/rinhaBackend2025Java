e5fbdcd9bd686408cf9c09646be3cef56305b71e
GustavoTBett <gustavotaufembachbtt@gmail.com>
2025-07-27 01:45:24 -0300

    Corrigido pipe

--- rinhaBackend2025Consumer/src/test/java/bett/gustavo/rinhaBackend2025Consumer/RinhaBackend2025ConsumerApplicationTests.java
+++ rinhaBackend2025Consumer/src/test/java/bett/gustavo/rinhaBackend2025Consumer/RinhaBackend2025ConsumerApplicationTests.java
@@ -2,8 +2,10 @@ package bett.gustavo.rinhaBackend2025Consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RinhaBackend2025ConsumerApplicationTests {
 
 	@Test
--- rinhaBackend2025Processor/src/test/java/bett/gustavo/rinhaBackend2025Processor/RinhaBackend2025ProcessorApplicationTests.java
+++ rinhaBackend2025Processor/src/test/java/bett/gustavo/rinhaBackend2025Processor/RinhaBackend2025ProcessorApplicationTests.java
@@ -2,8 +2,10 @@ package bett.gustavo.rinhaBackend2025Processor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RinhaBackend2025ProcessorApplicationTests {
 
 	@Test
