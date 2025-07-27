1d4edb00cf840096488d1932ec7b8c9a227597ca
GustavoTBett <gustavotaufembachbtt@gmail.com>
2025-07-27 01:21:07 -0300

    Atualizado para pegar payment processor correto e outras confs

--- rinhaBackend2025Consumer/src/main/java/bett/gustavo/rinhaBackend2025Consumer/config/ApiServiceConfig.java
+++ rinhaBackend2025Consumer/src/main/java/bett/gustavo/rinhaBackend2025Consumer/config/ApiServiceConfig.java
@@ -1,6 +1,7 @@
 package bett.gustavo.rinhaBackend2025Consumer.config;
 
 import bett.gustavo.rinhaBackend2025Consumer.service.ApiService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,13 +9,19 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class ApiServiceConfig {
 
+    @Value("PAYMENT_PROCESSOR_URL_DEFAULT")
+    private static String PAYMENT_PROCESSOR_URL_DEFAULT;
+
+    @Value("PAYMENT_PROCESSOR_URL_FALLBACK")
+    private static String PAYMENT_PROCESSOR_URL_FALLBACK;
+
     @Bean
     public ApiService defaultApiService(WebClient.Builder builder) {
-        return new ApiService(builder.baseUrl("http://localhost:8001"));
+        return new ApiService(builder.baseUrl(PAYMENT_PROCESSOR_URL_DEFAULT));
     }
 
     @Bean
     public ApiService fallbackApiService(WebClient.Builder builder) {
-        return new ApiService(builder.baseUrl("http://localhost:8002"));
+        return new ApiService(builder.baseUrl(PAYMENT_PROCESSOR_URL_FALLBACK));
     }
 }
