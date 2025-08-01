89bd78cd2d1eee12fec323a76678d3f7048f4140
GustavoTBett <gustavotaufembachbtt@gmail.com>
2025-07-31 21:46:25 -0300

    tirando testes

--- rinhaBackend2025Consumer/src/test/java/bett/gustavo/rinhaBackend2025Consumer/RinhaBackend2025ConsumerApplicationTests.java
+++ rinhaBackend2025Consumer/src/test/java/bett/gustavo/rinhaBackend2025Consumer/RinhaBackend2025ConsumerApplicationTests.java
@@ -1,15 +1,15 @@
-package bett.gustavo.rinhaBackend2025Consumer;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")
-class RinhaBackend2025ConsumerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package bett.gustavo.rinhaBackend2025Consumer;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class RinhaBackend2025ConsumerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
